415ffda2e180a65802bd32057756d1f4257d1279
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-02-18 10:59:22 -0300

    Updates

---
+++ precache-manifest.d293e136e7fe3da89205bdd28b264166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83f83f9f5e99f01f5f6f9094ed3b59c",
+    "url": "/personal-wallet/index.html"
+  },
+  {
+    "revision": "98ef4af8808115d49bf8",
+    "url": "/personal-wallet/static/css/main.c5e7324a.chunk.css"
+  },
+  {
+    "revision": "0cc569e8a13959d3ecf8",
+    "url": "/personal-wallet/static/js/2.3c3c397a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/personal-wallet/static/js/2.3c3c397a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ef4af8808115d49bf8",
+    "url": "/personal-wallet/static/js/main.9232ffc3.chunk.js"
+  },
+  {
+    "revision": "073c67cbc04edf15473e",
+    "url": "/personal-wallet/static/js/runtime-main.68d7514e.js"
+  }
+]);
